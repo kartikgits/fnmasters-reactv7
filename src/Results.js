d66c32559bd71be9f847a1d2a0d05eaba0801fce
kartikeya kapparwan <kartik.kapparwan1@gmail.com>
2022-07-10 09:45:52 +0530

    Component composition

---
+++ src/Results.js
@@ -0,0 +1,29 @@
+import Pet from "./Pet";
+
+// Only a JS expression goes inside {}
+// An expression in JS is whatever you can put into RHS of an =
+
+const Results = ({pets}) => {
+    return (
+        <div>
+            {!pets.length ? (
+                    <h1>No Pets Found</h1>
+                ) : (
+                    pets.map(pet => (
+                        <Pet 
+                            name={pet.name} 
+                            animal={pet.animal} 
+                            breed={pet.breed} 
+                            images={pets.images}
+                            location={`${pet.city}, ${pet.state}`}
+                            id={pet.id}
+                            key={pet.id}
+                        />
+                    ))
+                )
+            }
+        </div>
+    );
+};
+
+export default Results;
