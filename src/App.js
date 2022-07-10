da2cd18567dba39ca8fcd3d2778ebdd93d2cf48a
kartikeya kapparwan <kartik.kapparwan1@gmail.com>
2022-07-10 16:15:18 +0530

    use React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Routs, Route, Routes, Link } from "react-router-dom";
+import { StrictMode } from "react";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 // Use PARCEL for production (preferably for development also)
 
 const App = () => {
 	return (
-		<div>
-			<h1>Adopt Me!</h1>
-			<SearchParams />
-		</div>
+		<StrictMode>
+			<BrowserRouter>
+				<header>
+					<Link to="/">Adopt Me!</Link>
+				</header>
+				<Routes>
+					<Route path="/details/:id" element={<Details />} />
+					<Route path="/" element={<SearchParams />} />
+				</Routes>
+			</BrowserRouter>
+		</StrictMode>
 	)
 }
 
